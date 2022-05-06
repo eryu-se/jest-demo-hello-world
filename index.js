45525832ffce48409a3d9d57ac829e60ddba7228
Erdal Yusuf <97586365+eryu-se@users.noreply.github.com>
2022-05-06 11:05:32 +0200

    add new files and folders

---
+++ index.js
@@ -0,0 +1,7 @@
+function practiceTest(phrase) {
+    return phrase + "!";
+  }
+  
+  module.exports = {
+    practiceTest,
+  }
---
+++ index.test.js
@@ -0,0 +1,14 @@
+const { practiceTest } = require("./index");
+
+describe("Test for initial Jest setup.", () => {
+    describe("practiceTest", () => { 
+        // Testing suites can be used to organize your test responses. 
+        // If all tests within the suite pass, then only the text from the highest level describe will be rendered to the screen. 
+        // Every level that fails will have more and more detailed information if you set it up that way.
+        test("Given 'Hello World!', return 'Hello World!'", () => {
+            const received = "Hello World!";
+            const expected = "Hello World!";
+            expect(practiceTest(argument)).toBe(expected);
+        });
+    });
+});
